093c4f1f661490285ff1071bc9b1e61c8663d490
Robbe De Bie <robbe.debie@student.ucll.be>
2020-11-15 16:00:40 +0100

    Added image to RecipeInformationActivity.kt and made summary scrollable

--- app/src/main/java/com/example/chef_louiardie/RecipeInformationActivity.kt
+++ app/src/main/java/com/example/chef_louiardie/RecipeInformationActivity.kt
@@ -1,14 +1,19 @@
 package com.example.chef_louiardie
 
+import android.media.Image
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.text.Html.FROM_HTML_MODE_COMPACT
+import android.text.method.ScrollingMovementMethod
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import org.json.JSONObject
 import java.net.URL
+
 
 class RecipeInformationActivity : AppCompatActivity() {
 
@@ -18,12 +23,19 @@ class RecipeInformationActivity : AppCompatActivity() {
         setContentView(R.layout.recipe_information)
         val titel = findViewById<TextView>(R.id.titel)
         val summary = findViewById<TextView>(R.id.summary)
+        val recipeInfoImage = findViewById<ImageView>(R.id.recipeInfoImage)
+        summary.movementMethod = ScrollingMovementMethod()
 
         Thread {
-            val result = URL("https://api.spoonacular.com/recipes/" + intent.getStringExtra("id") + "/summary?apiKey=47d5a6ad23494cf696007384ca0524cd").readText()
+            val result = URL("https://api.spoonacular.com/recipes/" + intent.getStringExtra("id") + "/information?apiKey=47d5a6ad23494cf696007384ca0524cd").readText()
             runOnUiThread {
                 titel.text = JSONObject(result).getString("title")
-                summary.text = Html.fromHtml(JSONObject(result).getString("summary"),FROM_HTML_MODE_COMPACT)
+                summary.text = Html.fromHtml(
+                    JSONObject(result).getString("summary"),
+                    FROM_HTML_MODE_COMPACT
+                )
+                val url = JSONObject(result).getString("image")
+                Picasso.get().load(url).into(recipeInfoImage)
             }
         }.start()
 
