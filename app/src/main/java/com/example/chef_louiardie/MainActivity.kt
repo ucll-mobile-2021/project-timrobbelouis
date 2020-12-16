16b7e13c461fbd4ce937bb5d9909db8a19f7db94
Robbe De Bie <robbe.debie@student.ucll.be>
2020-12-16 11:56:15 +0100

    button leh

--- app/src/main/java/com/example/chef_louiardie/MainActivity.kt
+++ app/src/main/java/com/example/chef_louiardie/MainActivity.kt
@@ -13,25 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
-        val button = findViewById<Button>(R.id.button)
-        //voorbeeld code om request te sturen en values op de app te tonen
-        button.setOnClickListener {
-            Thread {
-                //Do some Network Request
-                val result = URL("https://api.spoonacular.com/recipes/complexSearch?apiKey=47d5a6ad23494cf696007384ca0524cd&query=pasta&excludeIngredients=tuna").readText()
-                println(result)
-                val resultJson = JSONObject(result)
-                val resultresultJson = resultJson.getJSONArray("results")
-//                val end = resultresultJson.length()-1
-//                for(x in 0..end){
-//                    println(JSONObject(resultresultJson.get(x).toString()).get("image"))
-//                }
-                val intent = Intent(this, ReceptenActivity::class.java).apply {
-                    putExtra("recepten", resultresultJson.toString())
-                }
-                startActivity(intent)
-            }.start()
-        }
+
 
         // search by ingredient
         val searchIngredientRecipe = findViewById<TextView>(R.id.searchByIngredients)
