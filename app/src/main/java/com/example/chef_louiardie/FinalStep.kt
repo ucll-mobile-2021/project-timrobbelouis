49029753146363775d117bd7cab59f1c72ce5036
Robbe De Bie <robbe.debie@student.ucll.be>
2020-12-20 14:29:07 +0100

    Poo

--- app/src/main/java/com/example/chef_louiardie/FinalStep.kt
+++ app/src/main/java/com/example/chef_louiardie/FinalStep.kt
@@ -9,9 +9,6 @@ class FinalStep : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.finish_step)
-        var mediaPlayer: MediaPlayer? = null
-        mediaPlayer = MediaPlayer.create(this, R.raw.congrats)
-        mediaPlayer.start()
     }
 
 
--- app/src/main/java/com/example/chef_louiardie/MainActivity.kt
+++ app/src/main/java/com/example/chef_louiardie/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val actionbar = supportActionBar
         actionbar?.hide()
         val properties = Properties()
-        properties.put("apikey", "61c6cdff38df4291862c41a13f16e51e")
+        properties.put("apikey", "f57ead542f804aa4bb76336783b870e0")
         System.setProperties(properties)
         val apikey = System.getProperties().getProperty("apikey")
         // search by ingredient
